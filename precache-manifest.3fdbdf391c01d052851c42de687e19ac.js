ca2a78b83c5a04637cb5300a40175d1d5b74a965
Vincent <vincentwhwoo@gmail.com>
2020-06-03 13:46:11 -0700

    Updates

---
+++ precache-manifest.3fdbdf391c01d052851c42de687e19ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012cecb5ef05d31c5c9cddf7b97908ec",
+    "url": "/chord-paper-fe/index.html"
+  },
+  {
+    "revision": "7791662d47f87d1337c3",
+    "url": "/chord-paper-fe/static/css/main.dddde6a1.chunk.css"
+  },
+  {
+    "revision": "433b0f63c7ac3e8e79d0",
+    "url": "/chord-paper-fe/static/js/2.2b861a1c.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/chord-paper-fe/static/js/2.2b861a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7791662d47f87d1337c3",
+    "url": "/chord-paper-fe/static/js/main.70393314.chunk.js"
+  },
+  {
+    "revision": "82d5071ff5416278aa5e",
+    "url": "/chord-paper-fe/static/js/runtime-main.3f6cd68b.js"
+  },
+  {
+    "revision": "3caa9627fc38cb4ded46c15997581c8e",
+    "url": "/chord-paper-fe/static/media/PoriChord.3caa9627.woff2"
+  }
+]);
