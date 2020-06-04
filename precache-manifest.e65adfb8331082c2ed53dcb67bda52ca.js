789aa5198cd2ec8eb1df7310fbba893c2bfee08c
veedubyou <veedubyou@users.noreply.github.com>
2020-06-04 10:01:29 +0000

    deploy: eac732a4896de55029752c68174eae3c3daeae5b

---
+++ precache-manifest.e65adfb8331082c2ed53dcb67bda52ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99206733ef2400a0e5eb41392489cd56",
+    "url": "/chord-paper-fe/index.html"
+  },
+  {
+    "revision": "8f4defad569e3eacc3c4",
+    "url": "/chord-paper-fe/static/css/main.dddde6a1.chunk.css"
+  },
+  {
+    "revision": "24aa23c6abc8adb06157",
+    "url": "/chord-paper-fe/static/js/2.8004f446.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/chord-paper-fe/static/js/2.8004f446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4defad569e3eacc3c4",
+    "url": "/chord-paper-fe/static/js/main.c9b3ce70.chunk.js"
+  },
+  {
+    "revision": "82d5071ff5416278aa5e",
+    "url": "/chord-paper-fe/static/js/runtime-main.3f6cd68b.js"
+  },
+  {
+    "revision": "3caa9627fc38cb4ded46c15997581c8e",
+    "url": "/chord-paper-fe/static/media/PoriChord.3caa9627.woff2"
+  }
+]);
