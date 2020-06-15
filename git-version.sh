d3d4c5d69bdb84d6ef5bf569b6462491c154e93d
Phil <philipchwong@gmail.com>
2020-06-15 03:17:38 +0000

    changed git-version.sh to make 2 files

--- git-version.sh
+++ git-version.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-GVF=GIT-VERSION-FILE
+GVF=./src/GIT-VERSION-FILE.js
 DEF_VER=v2.27.0
+GVF2=GIT-VERSION-FILE
 
 LF='
 '
@@ -10,31 +11,32 @@ LF='
 # then try git-describe, then default.
 if test -f version
 then
-	VN=$(cat version) || VN="$DEF_VER"
+        VN=$(cat version) || VN="$DEF_VER"
 elif test -d ${GIT_DIR:-.git} -o -f .git &&
-	VN=$(git describe --match "v[0-9]*" HEAD 2>/dev/null) &&
-	case "$VN" in
-	*$LF*) (exit 1) ;;
-	v[0-9]*)
-		git update-index -q --refresh
-		test -z "$(git diff-index --name-only HEAD --)" ||
-		VN="$VN-dirty" ;;
-	esac
+        VN=$(git describe --match "v[0-9]*" HEAD 2>/dev/null) &&
+        case "$VN" in
+        *$LF*) (exit 1) ;;
+        v[0-9]*)
+                git update-index -q --refresh
+                test -z "$(git diff-index --name-only HEAD --)" ||
+                VN="$VN-dirty" ;;
+        esac
 then
-	VN=$(echo "$VN" | sed -e 's/-/./g');
+        VN=$(echo "$VN" | sed -e 's/-/./g');
 else
-	VN="$DEF_VER"
+        VN="$DEF_VER"
 fi
 
 VN=$(expr "$VN" : v*'\(.*\)')
 
 if test -r $GVF
 then
-	VC=$(sed -e 's/^//' <$GVF)
+        VC=$(sed -e 's/^//' <$GVF)
 else
-	VC=unset
+        VC=unset
 fi
 test "$VN" = "$VC" || {
-	echo >&2 "$VN"
-	echo "$VN" >$GVF
+        echo >&2 "$VN"
+        echo "const version='$VN'; export default version;" >$GVF
+        echo "$VN" > $GVF2
 }
