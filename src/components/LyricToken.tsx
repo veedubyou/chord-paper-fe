043f06e3df3e812b70df0b7ddea8203dd039b234
Vincent <vincentwhwoo@gmail.com>
2020-06-15 22:00:00 -0700

    wip

--- src/components/LyricToken.tsx
+++ src/components/LyricToken.tsx
@@ -1,5 +1,7 @@
 import { Box, Theme, withStyles, Typography } from "@material-ui/core";
 import { TypographyProps } from "@material-ui/core";
+import { DataTestID } from "../common/DataTestID";
+import { isWhitespace } from "../common/Whitespace";
 
 export const lyricTypographyVariant: "h6" = "h6";
 
@@ -32,8 +34,33 @@ export const HighlightableTokenBox = withStyles((theme: Theme) => ({
     },
 }))(Box);
 
-interface LyricTokenProps extends TypographyProps {
+interface LyricTokenProps
+    extends Omit<TypographyProps, "variant" | "display">,
+        DataTestID {
     children: string;
 }
 
-const LyricToken: React.FC<{}> = (): JSX.Element => {};
+const LyricToken: React.FC<LyricTokenProps> = (
+    props: LyricTokenProps
+): JSX.Element => {
+    const typographyProps = {
+        variant: lyricTypographyVariant,
+        display: "inline" as "inline",
+    };
+
+    const TypographyComponent = isWhitespace(props.children)
+        ? SpaceTypography
+        : WordTypography;
+
+    const styleFirstToken = blockHasChord && index === 0;
+
+    const lyricBlock = (
+        <Typography
+            {...typographyProps}
+            className={lyricClass}
+            data-testid={`Token-${index}`}
+        >
+            {lyric}
+        </Typography>
+    );
+};
