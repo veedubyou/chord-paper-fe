9aac3cf8361325baa94be3807509d9eccb6dbc0d
Vincent <vincentwhwoo@gmail.com>
2020-07-05 20:23:40 -0700

    more clean up

--- src/test/paste.test.tsx
+++ src/test/paste.test.tsx
@@ -1,30 +1,11 @@
-import React from "react";
+import { act, cleanup, fireEvent, render } from "@testing-library/react";
+import { chordPaperFromLyrics } from "./common";
 import {
-    render,
-    fireEvent,
-    waitForElementToBeRemoved,
-    cleanup,
-    Matcher,
-    act,
-} from "@testing-library/react";
-
-import { ThemeProvider, createMuiTheme } from "@material-ui/core";
-
-import {
-    matchLyric,
-    lyricsInElement,
-    FindByTestIdChainFn,
     ExpectChordAndLyricFn,
+    FindByTestIdChainFn,
     getExpectChordAndLyric,
     getFindByTestIdChain,
 } from "./matcher";
-import { enterKey } from "./userEvent";
-import ChordPaper from "../components/edit/ChordPaper";
-import { SnackbarProvider } from "notistack";
-import { ChordSong } from "../common/ChordModel/ChordSong";
-import { ChordLine } from "../common/ChordModel/ChordLine";
-import { ChordBlock } from "../common/ChordModel/ChordBlock";
-import { chordPaperFromLyrics } from "./common";
 
 afterEach(cleanup);
 
