363756c63002f461c460c4de68fba24770f0bec3
veedubyou <veedubyou@users.noreply.github.com>
2020-07-19 08:03:02 +0000

    deploy: b74ebe320c15392491cc1eac55958a1661226d04

--- precache-manifest.edbf3fafb5318acb26fdee1a30a27ffa.js
+++ precache-manifest.edbf3fafb5318acb26fdee1a30a27ffa.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea75eed496ed694b1fc3ad38df5f4bcc",
+    "revision": "7b0abdf9a658155af26c1c5435ea64c3",
     "url": "/chord-paper-fe/index.html"
   },
   {
-    "revision": "72dce388eabbbefde575",
+    "revision": "4a73a1198fd7d089daba",
     "url": "/chord-paper-fe/static/css/main.bea93a8b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chord-paper-fe/static/js/2.cb6e43af.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "72dce388eabbbefde575",
-    "url": "/chord-paper-fe/static/js/main.5593d2c1.chunk.js"
+    "revision": "4a73a1198fd7d089daba",
+    "url": "/chord-paper-fe/static/js/main.3ceb2705.chunk.js"
   },
   {
     "revision": "82d5071ff5416278aa5e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chord-paper-fe/precache-manifest.de389991dc128703ba297fda2137e467.js"
+  "/chord-paper-fe/precache-manifest.edbf3fafb5318acb26fdee1a30a27ffa.js"
 );
 
 self.addEventListener('message', (event) => {
