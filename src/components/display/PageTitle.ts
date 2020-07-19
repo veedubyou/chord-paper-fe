ea4184138e418c933254c6f48e232cb772367eb8
Vincent <vincentwhwoo@gmail.com>
2020-07-18 19:31:48 -0700

    slightly more idiomatic

--- src/components/display/PageTitle.ts
+++
@@ -1,9 +0,0 @@
-import { ChordSong } from "../../common/ChordModel/ChordSong";
-
-export const pageTitle = (song: ChordSong): string => {
-    if (song.metadata.title === "") {
-        return "Chord Paper";
-    }
-
-    return `${song.metadata.title} - Chord Paper`;
-};
