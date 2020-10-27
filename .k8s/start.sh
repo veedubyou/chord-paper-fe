5dd6be4b6d07d5432499880a1f1da623f133d0eb
pw1124 <56487540+pw1124@users.noreply.github.com>
2020-10-27 14:03:48 -0700

    add runner, change production server (#149)
    Co-authored-by: Phil <phil@phil>

--- .k8s/start.sh
+++ .k8s/start.sh
@@ -2,6 +2,6 @@
 
 # Convenience script to deploy everything
 
-microk8s kubectl apply -f "deploy.yml"
-microk8s kubectl apply -f "service.yml"
-watch microk8s kubectl get all
+kubectl apply -f "deploy.yml"
+kubectl apply -f "service.yml"
+watch kubectl get all
