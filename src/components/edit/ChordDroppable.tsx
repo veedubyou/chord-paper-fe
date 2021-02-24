6e9c44b5e57f2a20fd2389c42b753e4bdc579245
Vincent <vincentwhwoo@gmail.com>
2021-02-24 00:32:25 -0800

    command/ctrl drag and drop implemented

--- src/components/edit/ChordDroppable.tsx
+++ src/components/edit/ChordDroppable.tsx
@@ -29,9 +29,62 @@ interface ClassNameable {
     className?: string;
 }
 
+interface ChordDropTargetProps {
+    children: (isOver: boolean) => React.ReactElement<ClassNameable>;
+    onDropped: ChordDroppableProps["onDropped"];
+}
+
+interface DropResult {
+    isOver: boolean;
+}
+
+const ChordDropTarget: React.FC<ChordDropTargetProps> = (
+    props: ChordDropTargetProps
+) => {
+    const [{ isOver }, dropRef] = useDrop<DNDChord, DNDChord, DropResult>({
+        accept: DNDChordType,
+        drop: (droppedChord: DNDChord, monitor: DropTargetMonitor) => {
+            const dropResult = monitor.getDropResult();
+
+            // the drop effect (i.e. move vs copy) can only be retrieved from the drop result,
+            // which is the object that is returned by this fn (drop), with dropEffect shimmed in
+            //
+            // the drop result is always null the first time and therefore the drop effect cannot be
+            // retrieved on the first interception of the drop event
+            //
+            // this handler is meant to be called twice - the first time where drop result is null
+            // and the second time where drop result is not null, and the drop effect can be found
+            //
+            // this code is coupled with the returned component below, where two nested <ChordDropTarget>s are returned
+            // to induce a valid drop result
+            if (dropResult !== null && !droppedChord.handled) {
+                droppedChord.handled = true;
+                const isCopyAction: boolean = dropResult.dropEffect === "copy";
+
+                props.onDropped(
+                    droppedChord.chord,
+                    droppedChord.sourceBlockID,
+                    isCopyAction
+                );
+            }
+
+            return droppedChord;
+        },
+        collect: (monitor: DropTargetMonitor): DropResult => ({
+            isOver: monitor.isOver({ shallow: true }),
+        }),
+    });
+
+    return <RootRef rootRef={dropRef}>{props.children(isOver)}</RootRef>;
+};
+
 interface ChordDroppableProps {
     children: React.ReactElement<ClassNameable>;
-    onDropped: (newChord: string, sourceBlockID: IDable<ChordBlock>) => void;
+    onDropped: (
+        newChord: string,
+        sourceBlockID: IDable<ChordBlock>,
+        copyAction: boolean
+    ) => void;
     hoverableClassName?: string;
     dragOverClassName?: string;
 }
@@ -39,32 +92,34 @@ interface ChordDroppableProps {
 const ChordDroppable: React.FC<ChordDroppableProps> = (
     props: ChordDroppableProps
 ) => {
-    const [{ isOver }, dropRef] = useDrop<DNDChord, DNDChord, any>({
-        accept: DNDChordType,
-        drop: (droppedChord: DNDChord) => {
-            if (!droppedChord.handled) {
-                droppedChord.handled = true;
-                props.onDropped(droppedChord.chord, droppedChord.sourceBlockID);
-            }
-            return droppedChord;
-        },
-        collect: (monitor: DropTargetMonitor) => ({
-            isOver: monitor.isOver({ shallow: true }),
-        }),
-    });
+    const childrenWithClassname = (
+        isOver: boolean
+    ): React.ReactElement<ClassNameable> => {
+        let childElem: React.ReactElement<ClassNameable> = props.children;
+        const childClassName: string | undefined = isOver
+            ? props.dragOverClassName
+            : props.hoverableClassName;
 
-    let childElem: React.ReactElement<ClassNameable> = props.children;
-    const childClassName: string | undefined = isOver
-        ? props.dragOverClassName
-        : props.hoverableClassName;
+        if (childClassName !== undefined) {
+            childElem = React.cloneElement(childElem, {
+                className: childClassName,
+            });
+        }
 
-    if (childClassName !== undefined) {
-        childElem = React.cloneElement(childElem, {
-            className: childClassName,
-        });
-    }
+        return childElem;
+    };
 
-    return <RootRef rootRef={dropRef}>{childElem}</RootRef>;
+    return (
+        // nested drop targets to get a drop result -
+        // see the drop handler in ChordDropTarget for a thorough explanation
+        <ChordDropTarget {...props}>
+            {() => (
+                <ChordDropTarget {...props}>
+                    {childrenWithClassname}
+                </ChordDropTarget>
+            )}
+        </ChordDropTarget>
+    );
 };
 
 export default ChordDroppable;
