c6ea50eb410325b27306450ffab71efaf7f2193d
veedubyou <veedubyou@users.noreply.github.com>
2021-07-21 18:38:36 +0000

    deploy: 721365441c7f8572fa0b699273feb247fa2fe191

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chord-paper-fe/precache-manifest.63f303d7433b9ef11cd57c9ed091c2a0.js"
+  "/chord-paper-fe/precache-manifest.633b004efe6b8a08dc03a043aaccc412.js"
 );
 
 self.addEventListener('message', (event) => {
