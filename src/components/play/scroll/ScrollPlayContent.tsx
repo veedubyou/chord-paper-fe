7fbaf0727ec72a2ee01451702dcba1f0a89f3266
veedubyou <vincentwhwoo@gmail.com>
2022-04-27 13:40:36 -0700

    navigate backwards (#303)

--- src/components/play/scroll/ScrollPlayContent.tsx
+++ src/components/play/scroll/ScrollPlayContent.tsx
@@ -3,12 +3,13 @@ import React, { useCallback, useRef } from "react";
 import { ChordSong } from "../../../common/ChordModel/ChordSong";
 import { noopFn } from "../../../common/PlainFn";
 import FocusedElement from "../common/FocusedElement";
+import { useNavigationKeys } from "../common/useNavigateKeys";
 import PlaySection, {
     ScrollableElement,
     sectionChordLines,
-    SectionedChordLines
+    SectionedChordLines,
+    Visibility,
 } from "./PlaySection";
-import { useNavigationKeys } from "../common/useNavigateKeys";
 
 interface ScrollPlayContentProps {
     song: ChordSong;
@@ -39,41 +40,81 @@ const ScrollPlayContent: React.FC<ScrollPlayContentProps> = (
         createEmptyRefs(sections.length)
     );
 
-    const getTopInViewSectionIndex = (): number | null => {
+    const getTopVisibleSection = (): {
+        index: number;
+        visibility: Visibility;
+    } | null => {
+        let firstInViewSection: {
+            index: number;
+            visibility: Visibility;
+        } | null = null;
         for (let i = 0; i < sectionRefs.current.length; i++) {
             const sectionVisibility = sectionRefs.current[i];
             if (sectionVisibility === null) {
                 continue;
             }
 
-            if (sectionVisibility.getInView() === "in-view") {
-                return i;
+            const visibility = sectionVisibility.getInView();
+
+            // return the section whose box top is near the top of the viewport if available
+            // this would best approximate what the user is seeing in terms of which
+            // section is at the top of the screen
+            if (visibility === "at-top") {
+                return {
+                    index: i,
+                    visibility: "at-top",
+                };
+            }
+
+            // otherwise, capture the first section that is in view
+            // we don't return this right away because the next section could be
+            // near the top of the viewport
+            // e.g. section A shows 5 px of height at the top of the screen
+            // and section B has their box top at 5 px from the top, then we
+            // want to classify section B as the "first section"
+            if (visibility === "in-view" && firstInViewSection === null) {
+                firstInViewSection = {
+                    index: i,
+                    visibility: "in-view",
+                };
             }
         }
 
-        return null;
+        return firstInViewSection;
     };
 
+    // scrollDown and scrollUp are unexpectedly not symmetrical functions
+    // explanation attached with each function
+    
+    // scrolls down to the next section head if it's in view
+    // otherwise scrolls down a bit (like page down)
+    // the idea is that the user can thoroughly navigate the entire song
+    // using just scrollDown without missing any lyrics/chords
     const scrollDown = (): boolean => {
-        const topInViewIndex = getTopInViewSectionIndex();
-        if (topInViewIndex === null) {
-            console.error("Can't find top index");
+        const currentSection = getTopVisibleSection();
+        if (currentSection === null) {
+            console.error(
+                "Can't find the section that's at the top of the viewport"
+            );
             return false;
         }
 
-        const hasNextSection = topInViewIndex < sectionRefs.current.length - 1;
+        const hasNextSection =
+            currentSection.index < sectionRefs.current.length - 1;
         const shouldScrollToNextSection: boolean = (() => {
             if (!hasNextSection) {
                 return false;
             }
 
-            const nextElement = sectionRefs.current[topInViewIndex + 1];
-            const nextElementInView = nextElement.getInView() === "in-view";
-            return nextElementInView;
+            const nextSectionRef =
+                sectionRefs.current[currentSection.index + 1];
+            return nextSectionRef.getInView() === "in-view";
         })();
 
         if (shouldScrollToNextSection) {
-            sectionRefs.current[topInViewIndex + 1].scrollIntoView();
+            const nextSectionRef =
+                sectionRefs.current[currentSection.index + 1];
+            nextSectionRef.scrollIntoView();
         } else {
             const windowHeight = document.documentElement.clientHeight;
             const scrollDistance = windowHeight * 0.75;
@@ -87,7 +128,36 @@ const ScrollPlayContent: React.FC<ScrollPlayContentProps> = (
         return true;
     };
 
-    useNavigationKeys(scrollDown, () => false);
+    // scrolls up to the section head above the viewport,
+    // which could correspond to the current or previous section
+    // unlike scrollDown, this may skip some lyrics/chords
+    // but the idea is that the user is "rewinding" through sections
+    // and not going through the song in reverse
+    // so therefore it's ok to skip some content
+    const scrollUp = (): boolean => {
+        const currentSection = getTopVisibleSection();
+        if (currentSection === null) {
+            console.error(
+                "Can't find the section that's at the top of the viewport"
+            );
+            return false;
+        }
+
+        const hasPreviousSection = currentSection.index > 0;
+
+        if (hasPreviousSection && currentSection.visibility === "at-top") {
+            const previousSectionRef =
+                sectionRefs.current[currentSection.index - 1];
+            previousSectionRef.scrollIntoView();
+        } else {
+            const currentSectionRef = sectionRefs.current[currentSection.index];
+            currentSectionRef.scrollIntoView();
+        }
+
+        return true;
+    };
+
+    useNavigationKeys(scrollDown, scrollUp);
 
     const playSections = sections.map(
         (sectionedChordLines: SectionedChordLines, sectionIndex: number) => {
