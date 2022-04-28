a9025e9c89a009d91a8798f7ac219b57c9912d64
veedubyou <vincentwhwoo@gmail.com>
2022-04-28 10:45:52 -0700

    Update SongFetcher.tsx (#305)

--- src/components/SongFetcher.tsx
+++ src/components/SongFetcher.tsx
@@ -14,7 +14,7 @@ import ErrorImage from "./display/ErrorImage";
 
 const GreyishBackdrop = withStyles({
     root: {
-        backgroundColor: fade(grey[200], 0.7),
+        backgroundColor: fade(grey[400], 0.5),
     },
 })(Backdrop);
 
