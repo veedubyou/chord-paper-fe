52c12f1a266a55ee4f539a16149257fd375d113d
veedubyou <vincentwhwoo@gmail.com>
2022-05-27 15:53:13 -0700

    prettier on all ts files

--- src/common/styledProps.ts
+++ src/common/styledProps.ts
@@ -1,12 +1,12 @@
 // extend your props with this and thread the className through to support MUI 5 styled() API
 // e.g. to support
 // const BlueYourComponent = styled(YourComponent)({
-//      color: "blue"   
+//      color: "blue"
 // });
 
 // interface YourComponentProps extends MUIStyledProps { ... }
 // const YourComponent = (props: YourComponentProps) => {
-//      return <Box className={props.className}> ... </Box> 
+//      return <Box className={props.className}> ... </Box>
 // }
 export interface MUIStyledProps {
     className?: string;
