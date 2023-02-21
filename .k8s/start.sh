b098d2be52461e54838df39f2f45231b02c075e5
Phil <phil@phil>
2023-02-21 00:00:51 -0800

    fixed the k8s start script

--- .k8s/start.sh
+++ .k8s/start.sh
@@ -2,6 +2,18 @@
 
 # Convenience script to deploy everything
 
-kubectl apply -f "deploy.yml"
-kubectl apply -f "service.yml"
+# gets latest image from dockerhub
+export LATEST_IMAGE=$(curl -L --fail "https://hub.docker.com/v2/repositories/pw1124/chord-fe/tags/?page_size=1000" | \
+        jq '.results | .[] | .name' -r | \
+        sed 's/latest//' | \
+        sort --version-sort | \
+        tail -n 1)
+
+printenv LATEST_IMAGE
+
+mkdir -p ./processed-yamls/ && cat ./deploy.yml | envsubst > ./processed-yamls/deploy.yml
+
+kubectl apply -f "./processed-yamls/deploy.yml"
+kubectl apply -f service.yml
+
 watch kubectl get all
