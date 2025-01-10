24ad0a62df4b63d3c0bf035790802a9f3da6e895
veedubyou <vincentwhwoo@gmail.com>
2025-01-09 22:47:56 -0800

    Update install-deps.sh

--- .github/scripts/install-deps.sh
+++ .github/scripts/install-deps.sh
@@ -7,6 +7,6 @@ sudo apt-get update && sudo apt-get install -y libssl-dev pkg-config \
         && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list \
         && sudo apt-get update \
         && sudo apt-get install -y yarn \
-        && curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - \
+        && curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - \
         && sudo apt-get install -y nodejs \
 
